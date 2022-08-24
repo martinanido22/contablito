d23881d155493f9360f5498e777a3b346034acbe
martinanido22 <martin.anido.22@gmail.com>
2022-08-24 11:50:40 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let tVenta = 0
-let tCompra = 0
+let totalVenta = 0
+let totalCompra = 0
 
+<<<<<<< Updated upstream
+=======
+document.getElementById("tipoVentas").innerHTML = tipoVenta;
+
+document.getElementById("tipoCompras").innerHTML = tipoCompra;
+
+>>>>>>> Stashed changes
 function iva() {
     if (ivaBasico.checked == true) {
         return 22;
@@ -23,7 +30,20 @@ function total() {
 }
 
 function tIva() {
+<<<<<<< Updated upstream
     return subTotal + (subTotal/100)*iva();
+=======
+    return subTotal + ((subTotal / 100) * iva());
+}
+
+function total() {
+    if (tipoTotal() == "Compra") {
+        return totalCompra += subTotal;
+    }
+    if (tipoTotal() == "Venta") {
+        return totalVente += subTotal;
+    }
+>>>>>>> Stashed changes
 }
 
 function iva
@@ -34,9 +54,14 @@ document.getElementById("").addEventListener("click", () => {
     let compra = document.getElementById("");
     let venta = document.getElementById("");
 
+<<<<<<< Updated upstream
     let ivaBasico = document.getElementById("");
     let ivaMinimo = document.getElementById("");
     let ivaExento = document.getElementById("");
+=======
+    let compra = document.getElementById("tipoCompra");
+    let venta = document.getElementById("tipoVenta");
+>>>>>>> Stashed changes
 
     let subTotal = document.getElementById("").value;
 
